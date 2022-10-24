2b0706cc6452e03e26edf474e9f92a21c39fd1ec
MaxVillagomez <maxvillagomez5141@gmail.com>
2022-10-24 17:41:52 -0400

    updated portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,11 +5,19 @@ const Portfolio = () => {
     return (
         <div className="portfolio-container">
             <p>Click on an image to be directed to deployed version of the project!</p>
-            <div className="coding-companions-container">
-                <a href="https://code-companions.herokuapp.com" target="_blank">
-                    <img className="coding-companions-image" src={codingCompanions}/>
-                </a>
-                <p>Grace-Shopper, an e-commerce website.</p>
+            <div className="portfolio-items">
+                <div className="coding-companions-container">
+                    <a href="https://code-companions.herokuapp.com" target="_blank">
+                        <img className="coding-companions-image" src={codingCompanions}/>
+                    </a>
+                    <p>Grace-Shopper, an e-commerce website.</p>
+                </div>
+                <div className="coding-companions-container">
+                    <p>Project Update Coming Soon</p>
+                </div>
+                <div className="coding-companions-container">
+                    <p>Project Update Coming Soon</p>
+                </div>
             </div>
         </div>
     )
